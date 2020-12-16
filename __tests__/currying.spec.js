d98a76c91819b23faabc34fad9d4007d569b3633
RaresAil <ailincairares1@gmail.com>
2020-12-16 17:59:59 +0200

    refactor test for currying

--- __tests__/currying.spec.js
+++ __tests__/currying.spec.js
@@ -3,17 +3,32 @@ const { expect } = require('chai');
 const app = require('../app/currying');
 
 describe('App | Currying & Function Composition', () => {
+  const numbers = [2, 4, 8];
   const expectedOutput = 14;
 
-  it(`The sum of 2, 4, 8 should be ${expectedOutput} with 'sum'`, () => {
-    expect(app.sum(2, 4, 8)).to.be.deep.equal(expectedOutput);
+  it(`The sum of ${numbers.join(
+    ', '
+  )} should be ${expectedOutput} with 'sum'`, () => {
+    const sum = app.sum(...numbers);
+
+    expect(sum).to.be.deep.equal(expectedOutput);
   });
 
-  it(`The sum of 2, 4, 8 should be ${expectedOutput} with 'curryingSum'`, () => {
-    expect(app.curryingSum(2)(4)(8)).to.be.deep.equal(expectedOutput);
+  it(`The sum of ${numbers.join(
+    ', '
+  )} should be ${expectedOutput} with 'curryingSum'`, () => {
+    const sum = app.curryingSum(numbers[0])(numbers[1])(numbers[2]);
+
+    expect(sum).to.be.deep.equal(expectedOutput);
   });
 
-  it(`The sum of 2, 4, 8 should be ${expectedOutput} with 'currying' and 'function composition'`, () => {
-    expect(app.currying(app.sum, 2)(4, 8)).to.be.deep.equal(expectedOutput);
+  it(`The sum of ${numbers.join(
+    ', '
+  )} should be ${expectedOutput} with 'currying' and 'function composition'`, () => {
+    const [_, ...restNumbers] = numbers;
+
+    const sum = app.currying(app.sum, numbers[0])(...restNumbers);
+
+    expect(sum).to.be.deep.equal(expectedOutput);
   });
 });
