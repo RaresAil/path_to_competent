279a3a61eec0588d3fa192e0e29a7b4bae8eb059
RaresAil <ailincairares1@gmail.com>
2020-12-16 15:21:23 +0200

    Polyfill function fromEntries for node v10

---
+++ __tests__/init.js
@@ -0,0 +1,12 @@
+const fromEntriesPolyfill = require('../polyfill/fromEntries');
+const define = require('define-properties');
+
+if (!Object.fromEntries) {
+  define(Object, {
+    fromEntries: fromEntriesPolyfill
+  }, {
+    fromEntries: function test() {
+      return Object.fromEntries !== fromEntriesPolyfill;
+    }
+  });
+}
---
+++ polyfill/fromEntries.js
@@ -0,0 +1,15 @@
+const AddEntriesFromIterable = require('es-abstract/2020/AddEntriesFromIterable');
+const CreateDataPropertyOrThrow = require('es-abstract/2020/CreateDataPropertyOrThrow');
+const RequireObjectCoercible = require('es-abstract/2020/RequireObjectCoercible');
+const ToPropertyKey = require('es-abstract/2020/ToPropertyKey');
+
+const adder = function addDataProperty(key, value) {
+  const O = this;
+  const propertyKey = ToPropertyKey(key);
+  CreateDataPropertyOrThrow(O, propertyKey, value);
+};
+
+module.exports = function fromEntries(iterable) {
+  RequireObjectCoercible(iterable);
+  return AddEntriesFromIterable({}, iterable, adder);
+};
