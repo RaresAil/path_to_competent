6533c7ed348b29e0a0ce9c14793bb554d25e5ace
RaresAil <ailincairares1@gmail.com>
2021-01-17 17:24:16 +0200

    tests(workers): Skip tests for Node v10

--- __tests__/workers.spec.js
+++ __tests__/workers.spec.js
@@ -1,35 +1,44 @@
 'use strict';
 
-const { expect, wa } = require('chai');
+try {
+  require('worker_threads');
 
-const app = require('../app/workers');
+  const { expect } = require('chai');
+  const app = require('../app/workers');
 
-describe('App | Workers', () => {
-  it('Create 2 hashes using different Workers', (done) => {
-    const start = Date.now();
-    let ms1 = Infinity;
-    let ms2 = Infinity;
+  describe('App | Workers', () => {
+    it('Create 2 hashes using different Workers', (done) => {
+      const start = Date.now();
+      let ms1 = Infinity;
+      let ms2 = Infinity;
 
-    app.createHashWorker('test', () => {
-      ms1 = Date.now() - start;
+      app.createHashWorker('test', () => {
+        ms1 = Date.now() - start;
+      });
+      app.createHashWorker('test', () => {
+        ms2 = Date.now() - start;
+      });
+
+      setTimeout(() => {
+        expect(ms1).to.be.lessThan(900);
+        expect(ms2).to.be.lessThan(900);
+        done();
+      }, 900);
     });
-    app.createHashWorker('test', () => {
-      ms2 = Date.now() - start;
-    });
 
-    setTimeout(() => {
-      expect(ms1).to.be.lessThan(900);
-      expect(ms2).to.be.lessThan(900);
-      done();
-    }, 900);
+    it('Create 2 hashes using the event loop', (done) => {
+      app.create2Hashes('test', (hashes) => {
+        expect(hashes[0].ms).to.be.lessThan(900);
+        expect(hashes[1].ms).to.be.greaterThan(hashes[0].ms * 1.5);
+        expect(hashes[1].ms).to.be.lessThan(3000);
+        done();
+      });
+    });
   });
-
-  it('Create 2 hashes using the event loop', (done) => {
-    app.create2Hashes('test', (hashes) => {
-      expect(hashes[0].ms).to.be.lessThan(900);
-      expect(hashes[1].ms).to.be.greaterThan(hashes[0].ms * 1.5);
-      expect(hashes[1].ms).to.be.lessThan(3000);
-      done();
-    });
-  });
-});
+} catch (e) {
+  if (e instanceof Error && e.code === 'MODULE_NOT_FOUND') {
+    describe.skip('App | Workers', () => {});
+  } else {
+    throw e;
+  }
+}
